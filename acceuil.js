9a2f698488a394901e34c1cf6e5609b4b7fd1a6a
WorldAirVirtuel <worldairvirtuel@gmail.com>
2024-08-13 13:05:39 +0200

    Add files via upload

---
+++ acceuil.js
@@ -0,0 +1,22 @@
+// scripts.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const counters = document.querySelectorAll('.counter-value');
+
+    counters.forEach(counter => {
+        const updateCounter = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+            const increment = target / 200;
+
+            if (count < target) {
+                counter.innerText = Math.ceil(count + increment);
+                setTimeout(updateCounter, 10);
+            } else {
+                counter.innerText = target;
+            }
+        };
+
+        updateCounter();
+    });
+});
